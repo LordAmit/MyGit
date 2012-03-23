067c9b601318f2640128f085dcf29a9e1de6ce58
lordamit <lordamit@gmail.com>
2012-03-22 18:05:08 -0700

    Third Comment!

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -10,7 +10,8 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Hello Universe!!!");
+            Console.ReadKey();
         }
     }
 }
